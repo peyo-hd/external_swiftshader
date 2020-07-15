f3ada421002a216fb564a8a1ced042095171a739
Jason Macnak <natsu@google.com>
2020-07-15 16:55:53 -0700

    Merge 'aosp/upstream-swiftshader-master' into 'aosp/master'
    ... to upgrade SwiftShader for Cuttlefish to support
    Gralloc3.
    
    Bug: b/158474511
    Bug: b/157902551
    Test: launch_cvd && cts
    Change-Id: I4b5f0d6a673f4a47d03235beaeac1bad42f89929
    Merged-In: I57967f739c9929fda8153d24bed77f6d79a08ad4

--- third_party/llvm-7.0/configs/darwin/include/llvm/Config/llvm-config.h
+++ third_party/llvm-7.0/configs/darwin/include/llvm/Config/llvm-config.h
@@ -23,12 +23,14 @@
 /* Target triple LLVM will generate code for by default */
 #if defined(__x86_64__)
 #define LLVM_DEFAULT_TARGET_TRIPLE "x86_64-apple-darwin"
+#elif defined(__aarch64__)
+#define LLVM_DEFAULT_TARGET_TRIPLE "arm64-apple-darwin"
 #else
 #error "unknown architecture"
 #endif
 
 /* Define if threads enabled */
-#define LLVM_ENABLE_THREADS 0
+#define LLVM_ENABLE_THREADS 1
 
 /* Has gcc/MSVC atomic intrinsics */
 #define LLVM_HAS_ATOMICS 1
@@ -36,6 +38,8 @@
 /* Host triple LLVM will be executed on */
 #if defined(__x86_64__)
 #define LLVM_HOST_TRIPLE "x86_64-apple-darwin"
+#elif defined(__aarch64__)
+#define LLVM_HOST_TRIPLE "arm64-apple-darwin"
 #else
 #error "unknown architecture"
 #endif
